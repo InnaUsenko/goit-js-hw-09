020da822cd9bd688f22aa27bba32fe4a9fe1cf66
InnaUsenko <innausenko8@gmail.com>
2023-08-08 17:17:25 +0000

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const myBody = document.querySelector('body');
+let timerId = null;
 
+stopBtn.setAttribute('disabled', '');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+startBtn.addEventListener('click', () => {
+  startBtn.setAttribute('disabled', '');
+  stopBtn.removeAttribute('disabled');
+  timerId = setInterval(() => {
+    myBody.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  myBody.removeAttribute('style');
+  stopBtn.setAttribute('disabled', '');
+  startBtn.removeAttribute('disabled');
+});
