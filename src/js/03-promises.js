70a44e1df64a4e83cb3ac2174f9f4e9a027f6f75
InnaUsenko <innausenko8@gmail.com>
2023-08-09 12:28:57 +0000

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
+import Notiflix from 'notiflix';
+
+const myForm = document.querySelector('.form');
+
+myForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+
+  const dataObj = {
+    delayInt: parseInt(delay.value),
+    stepInt: parseInt(step.value),
+    amountInt: parseInt(amount.value),
+  };
+
+  for (i = 0; i < dataObj.amountInt; i++) {
+    createPromise(i + 1, dataObj.delayInt + i * dataObj.stepInt)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+});
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+  return promise;
 }
